f46c01966d3eb47fcb4640189d72d8304911f03a
CHEN Dongmei <DCHEN@lcog.org>
2020-10-20 20:13:22 -0700

    double checked carbon intensity calculation

--- input/carbon_intensity.R
+++ input/carbon_intensity.R
@@ -41,6 +41,20 @@ get.pct.by.utility <- function(city){
 }
 
 get.pct.by.utility(city='EUG')
+# NAME  Areas_acre   Areas_pct
+# 1                  Lane Elec  1708.67086 0.047952954
+# 2               Blachly-Lane  1055.21079 0.029613939
+# 3                Emerald PUD   118.77832 0.003333451
+# 4  Springfield Utility Board    60.63404 0.001701662
+# 5                       EWEB 32688.93987 0.917397994
 get.pct.by.utility(city='SPR')
+# NAME Areas_acre    Areas_pct
+# 1                Emerald PUD  1265.1477 0.0819459810
+# 2                Emerald PUD    10.0901 0.0006535547
+# 3  Springfield Utility Board 13754.6328 0.8909132877
+# 4                       EWEB   408.9302 0.0264871765
 get.pct.by.utility(city='COB')
+# NAME Areas_acre Areas_pct
+# 1 Emerald PUD   757.4331         1
 
+# Electricity CI
