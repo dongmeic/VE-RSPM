98339b5d44eea951ed5b318cb12afe34848c3526
dongmeic <dongmeic@uoregon.edu>
2022-10-16 23:41:29 -0700

    added notes

--- run/nextgen/02_Run_CLMPO_Scenarios.R
+++ run/nextgen/02_Run_CLMPO_Scenarios.R
@@ -1,4 +1,4 @@
-# Objective: Run the version 2022-05-27 with the CLMPO scenarios inputs
+# Objective: Run VE 3.0 with the CLMPO scenarios inputs
 # By: Dongmei Chen (dchen@lcog.org)
 # October 14th, 2022 
 # Compare to T:\DCProjects\GitHub\VE-RSPM\run\firstrun\run_example_scenarios.R
